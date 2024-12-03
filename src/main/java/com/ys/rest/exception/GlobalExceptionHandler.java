2e4d0bcb04a54dfc887de148a0b86de73154dcd4
Yuliya_Seliukova <yuliya_seliukova@epam.com>
2024-12-03 10:18:52 +0300

    TASK 1. BUILDING A REST API

---
+++ src/main/java/com/ys/rest/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.ys.rest.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleResourceNotFoundException(
+            EntityNotFoundException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+}
